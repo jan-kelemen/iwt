b9772a993e47c21fc1a0131e99625909bc6d2492
Jan Kelemen <jkeleme@gmail.com>
2024-09-17 17:17:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# iwt (Imp Was already Taken) [![GitHub Build status](https://github.com/jan-kelemen/iwt/actions/workflows/ci.yml/badge.svg?branch=master)](https://github.com/jan-kelemen/iwt/actions/workflows/ci.yml)
+
+Imp Was already Taken.
+
---
+++ conanfile.py
@@ -0,0 +1,32 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+
+require_conan_version = ">=2.0"
+
+class IWTConan(ConanFile):
+    name = "iwt"
+    settings = "os", "compiler", "build_type", "arch"
+    version = "0.1"
+
+    exports_sources = "cmake", "src", "CMakeLists.txt", "LICENSE"
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[^3.27]")
+        self.test_requires("catch2/[^3.6.0]")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = "ConanPresets.json"
+        tc.generate()
+
+        cmake = CMakeDeps(self)
+        cmake.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
